d70ea2b404234146bced3e61d10050c7e70d4967
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-07-22 15:12:59 -0700

    use mocha

--- index.test.js
+++ index.test.js
@@ -1,16 +1,7 @@
 const { forEach, map } = require("./index");
 const assert = require("assert");
 
-const test = (desc, fn) => {
-  console.log("----", desc);
-  try {
-    fn();
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-
-test("Test for forEach", () => {
+it("Test for forEach", () => {
   let sum = 0;
   forEach([1, 2, 3], (value) => {
     sum += value;
@@ -19,7 +10,7 @@ test("Test for forEach", () => {
   assert.strictEqual(sum, 6);
 });
 
-test("Test for map", () => {
+it("Test for map", () => {
   const result = map([1, 2, 3], (value) => {
     return value * 2;
   });
