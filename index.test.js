6a018b332db447851d063fba07fbecd2e69af59d
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-07-22 13:59:17 -0700

    add test function

--- index.test.js
+++ index.test.js
@@ -1,24 +1,37 @@
 const { forEach, map } = require("./index");
 
-let sum = 0;
-forEach([1, 2, 3], (value) => {
-  sum += value;
+const test = (desc, fn) => {
+  console.log("----", desc);
+  try {
+    fn();
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+test("Test for forEach", () => {
+  let sum = 0;
+  forEach([1, 2, 3], (value) => {
+    sum += value;
+  });
+
+  if (sum !== 6) {
+    throw new Error("Expected summing array equal to 6");
+  }
 });
 
-if (sum !== 6) {
-  throw new Error("Expected summing array equal to 6");
-}
+test("Test for map", () => {
+  const result = map([1, 2, 3], (value) => {
+    return value * 2;
+  });
 
-const result = map([1, 2, 3], (value) => {
-  return value * 2;
+  if (result[0] !== 2) {
+    throw new Error(`Expected 2, but found ${result[0]}`);
+  }
+  if (result[1] !== 4) {
+    throw new Error(`Expected 4, but found ${result[1]}`);
+  }
+  if (result[2] !== 6) {
+    throw new Error(`Expected 6, but found ${result[2]}`);
+  }
 });
-
-if (result[0] !== 2) {
-  throw new Error(`Expected 2, but found ${result[0]}`);
-}
-if (result[1] !== 4) {
-  throw new Error(`Expected 4, but found ${result[1]}`);
-}
-if (result[2] !== 6) {
-  throw new Error(`Expected 6, but found ${result[2]}`);
-}
